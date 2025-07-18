8f23861a9daf20abf1f070616bd3a6e042a677b1
Cosmic AI <support@cosmicjs.com>
2025-07-18 14:18:54 -0700

    Update favicon emoji implementation in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,9 +46,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🚀</text></svg>" />
-      </head>
       <body className={inter.className}>
         <Header />
         <main className="min-h-screen">
